6cb452c662083b2faeded2cac601089e38ede21e
naraeseo <naraeseoez@gmail.com>
2022-12-28 14:37:17 +0900

    [new] #4.6 : Outlet 사용하기

--- src/screens/users/User.tsx
+++ src/screens/users/User.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 import { users } from '../../db';
 
 const User = () => {
@@ -6,9 +6,14 @@ const User = () => {
   console.log('userId :', userId);
 
   return (
-    <h1>
-      User with id '{userId}' is named: {users[Number(userId) - 1].name}
-    </h1>
+    <div>
+      <h1>
+        User with id '{userId}' is named: {users[Number(userId) - 1].name}
+      </h1>
+      <hr />
+      <Link to='followers'>See followers</Link>
+      <Outlet />
+    </div>
   );
 };
 
